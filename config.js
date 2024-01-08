dfceb4c2c0351913265c7bbaa1e0e2968a94b195
Utkarsh <utkarshjha.4009@gmail.com>
2024-01-08 16:36:54 +0530

    Port Adjustment

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 export const mongoDBURL='mongodb+srv://root:root@book-store-mern.iv3iur6.mongodb.net/books-collection?retryWrites=true&w=majority'
